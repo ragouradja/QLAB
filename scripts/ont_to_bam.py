c99a30e968734358efbcf635a71a7d185fe47c83
ragouradja <71189947+ragouradja@users.noreply.github.com>
2022-10-13 17:13:18 +0200

    Script with comments

--- scripts/ont_to_bam.py
+++ scripts/ont_to_bam.py
@@ -1,4 +1,4 @@
-""" Create bed file with boundings of reads"""
+""" Converting BED file to BAM file with Biseq mode visualization """
 
 
 
@@ -15,15 +15,8 @@ import polars as pl
 
 
 def get_args():
-    """Function to get all args
-    Arguments
-    ---------
+    """Function to get all args"""
 
-    
-    meth_read = os.path.abspath(sys.argv[1])
-    output_directory = os.path.abspath(sys.argv[2])
-    genome_path = os.path.abspath(sys.argv[3])
-    """
     parser = argparse.ArgumentParser(description="Generate bam file from ONT reads data",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--file","-f", help="Path to file with reads informations for one chromosome.", type=str, required=True, metavar="chr_file")
@@ -45,6 +38,25 @@ def get_args():
 
 
 def read_ref(reference_path, chrom, args):
+    """Read the genome
+    Arguments
+    ---------
+    reference_path : string
+        Path to the reference genome
+    chrom : List
+        List of chromosom to keep while reading the genome 
+        (can save some memory space if some chrom are not used)
+    args : List
+        List of arguments from get_args()
+        
+
+    Return
+    ------
+        genome : Dict
+            Genome in format : {"Chr1" : ["A","T","C"...],
+                                "Chr2" : [...]}
+    """
+
     genome = {}
     print("Reading genome...")
     with open(reference_path) as filin:
@@ -60,6 +72,22 @@ def read_ref(reference_path, chrom, args):
     return genome
 
 def get_chrs_path(args):
+    """Find path to all chromosomes bedfiles from chr1 bedfile path
+    using regular expression to replace 'chr1' with 'chr2','chr3' ...
+
+    This function is used only if you run with --all option.
+
+    Arguments
+    ---------
+    args : List
+        List of arguments from get_args()
+        
+
+    Return
+    ------
+        all_files : List
+            List containing existing paths to bedfiles for each chrom
+    """
     all_files = []
     path = os.path.abspath(args.file)
     print("Processing all chromosomes in parallel.")
@@ -78,11 +106,35 @@ def get_time(start):
     return round(time.time() - start,2)
 
 def get_dict(read_file, chrom_name):
+    """Read methylation data (for fast_mode only)
+    
+    Reading data using Polars which load data using multiprocessing.
+    Optimization of memory by using cheaper data type.
+    Groupby data by read then converting the dataframe into dictionary which use less memory.
+
+    Arguments
+    ---------
+    read_file : string
+        Path to the bedfile containing methylation information
+        per read and per position.
+    chrom_name : string
+        Chromosome name (only for printing informations)
+
+    Return
+    ------
+        df_dict : Dict
+            Methylation data in dict format : {Read1 : np.array(["chr","start","strand","state"]), 
+                                               Read2 : ...}
+            Remark : Data for each read are in numpy array which allow vectorial operations
+                     within one read (faster than classical operations).
+    """
+
     header = ["chr","start","start_in_read","read","strand","state"]
     start_time = time.time()
     print(f"{chrom_name} :: Loading dataframe...")
     start_pl = time.time()
-    meth_dtf = pl.read_csv(read_file, new_columns  = header, sep = "\t", has_header = False, low_memory = True , n_threads = 10).drop("start_in_read").to_pandas()
+    meth_dtf = pl.read_csv(read_file, new_columns  = header, sep = "\t", has_header = False,
+     low_memory = True , n_threads = 10).drop("start_in_read").to_pandas()
     start_opti = time.time()
     print(f"{chrom_name} :: Dataframe loaded in {get_time(start_pl)}s") 
     print(f"{chrom_name} :: Start memory optimisation") 
@@ -97,11 +149,32 @@ def get_dict(read_file, chrom_name):
     return df_dict
 
 def change_base(sequence, positions, base):
+    """Converting sequence to Biseq mode
+    
+    Arguments
+    ---------
+    sequence : List
+        Sequence to modify
+
+    positions : List
+        Position of unmethylated cytosines to modify
+    base : string
+        New base to replace cytosine
+        ('T' if + strand, 'A' if - strand)
+
+    Return
+    ------
+    sequence : List
+        Sequence modified 
+    """
+
     for i in positions:
         sequence[i] = base
     return sequence
 
 def write_sam(read, flag, chrom, start, seq, file_out):
+    """ Function to write one line in SAM file"""
+
     before_seq = '*\t0\t0'
     qual = 5
     len_seq = len(seq)
@@ -112,34 +185,96 @@ def write_sam(read, flag, chrom, start, seq, file_out):
 
     
 def create_sam_fast(df_dict, SAM_output, BAM_output, genome, args, chrom_name):
+    """Creating SAM file in Biseq mode (fast_mode and relatively high memory usage)
+    
+    Iterate through all keys of df_dict, which are all reads, and modify unmethylated cytosines.
+    The sequences modifed are not exactly the raw sequence of the read 
+        but directly extracted from the genome (to avoid dealing with insertion or deletion)
+
+    Arguments
+    ---------
+    df_dict : dict
+        Methylation data in dict format from get_dict()
+    SAM_output : string
+        Filename of SAM file
+    BAM_output : string
+        Filename of BAM file
+    genome : dict
+        Genome in dict format from read_ref()
+    args : list
+        List of arguments from get_args()
+        
+    chrom_name : string 
+        Chromosome name (only for printing informations)
+
+
+    """
+
+
     bases_strand = {"+":"T", "-":"A"}
     flag_strand = {"+":  1, "-" : 17}
     with open(SAM_output, "w") as sam_filout:
         for read in df_dict:
             read_lines = df_dict[read]
+            # Get methylation information for this particular read
             state = read_lines[:,3]
+            # Selecting unmethylated cytosines
             base_to_change = np.where(state == 0)[0]
             pos = read_lines[:,1]
             first_position = pos.min()
             last_position = pos.max() + 1
+            # Converting relative position to absolute position (starting at 0)
             position_in_seq = (pos - first_position)[base_to_change]
             strand = read_lines[0,2]
             chrom = f"Chr{read_lines[0,0]}"
+            # Base that will replace cytosines, depending on strand
             base = bases_strand[strand]
+            # Extracting sequence from genome dict
+            # The first (also for last) nucleotide in the sequence 
+            # will be the first (last) cytosine detected for a given read
             sequence = genome[chrom][first_position:last_position]
+            # Modifying sequence
             sequence = "".join(change_base(sequence, position_in_seq, base))
+            # Write one line in SAM file for this read with modified sequence
             write_sam(read, flag_strand[strand], chrom, first_position, sequence, sam_filout)
 
 
 
 def create_sam_slow(bedfile, SAM_output, BAM_output, genome, args, chrom_name):
-    
+    """Creating SAM file in Biseq mode (slow_mode and no memory usage)
+    Need to have bedfiles sorted by k4 k2 : By read and then by the position
+
+    Iterate through all lines of one chromosome file and modify the sequence.
+    Since we are not loading all the data into memory, we can parallelize
+        the reading and processing of all 5 bedfiles.
+    The sequences modifed are not exactly the raw sequence of the read 
+        but directly extracted from the genome (to avoid dealing with insertion or deletion)
+
+    Arguments
+    ---------
+    bedfile : string
+        Path to one bedfile to read and process
+    SAM_output : string
+        Filename of SAM file
+    BAM_output : string
+        Filename of BAM file
+    genome : dict
+        Genome in dict format from read_ref()
+    args : list
+        List of arguments from get_args()
+    chrom_name : string 
+        Chromosome name (only for printing informations)
+
+
+    """
     prev_read = ""
     act_read = ""
     bases_strand = {"+":"T", "-":"A"}
     flag_strand = {"+":  1, "-" : 17}
     pos_to_change = np.array([], dtype = np.int8)
 
+    # Same logic as for create_sam_fast() but here we iterate directly the bedfile
+    # To detect the end of one read, we need to keep in memory the last line ('prev' variables)
     with open(bedfile) as bed_filin, open(SAM_output, "a") as sam_filout:
         lines = bed_filin.readline()
         items_first = lines.split()
@@ -153,6 +288,7 @@ def create_sam_slow(bedfile, SAM_output, BAM_output, genome, args, chrom_name):
         for line in bed_filin:
             items = line.split()
             act_read = items[3]
+            # If we detect a new read, the sequence of the previous read is modified
             if prev_read != act_read:
                 last_position = prev_start + 1
                 base = bases_strand[prev_strand]
@@ -184,6 +320,7 @@ def create_sam_slow(bedfile, SAM_output, BAM_output, genome, args, chrom_name):
 
 
 def create_bam(bam_output, output_sam, args):
+    """Write BAM file from SAM file (from fast or slow mode)"""
 
     genome_basename =  os.path.basename(args.genome)
     genome_name = os.path.splitext(genome_basename)[0]
